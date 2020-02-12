75de81a9315a2ce183bbbefeb74f27667190121c
FaidhiGhazali <49259683+FaidhiGhazali@users.noreply.github.com>
2020-02-12 11:41:54 +0800

    Create Isogram.vue

---
+++ Isogram.vue
@@ -0,0 +1,37 @@
+<template lang="">
+    <v-container grid-list-lg>
+        <v-layout row wrap>
+          <v-card>
+            <span>{{ isogram(word) }}</span>
+          </v-card>
+        </v-layout>
+    </v-container>
+</template>
+
+<script>
+
+export default {
+    data() {
+        return {
+            word: 'moOse'
+        }
+    },
+  methods: {
+    isogram (str) {
+      var arr = str.toLowerCase().split("")
+      var inArr = []
+      for(var i =0; i<arr.length; i++){
+       if (!inArr[arr[i]]) {
+       	inArr[arr[i]] = arr[i];
+       } else {
+       	return false;
+       }
+      }
+      return true;
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
